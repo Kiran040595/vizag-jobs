abc9e695fc33d561c7b0106455ca43fb555e196c
Kiran040595 <kirankumar040595@gmail.com>
2026-04-26 20:45:25 +0530

    feat: Temporarily hide categories section
    - Comment out CategoriesSection component in HomePage
    - Clean up homepage layout by removing categories for now

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -101,7 +101,7 @@ export default function HomePage() {
       <HeroSection searchTerm={searchTerm} onSearch={setSearchTerm} />
 
       <main className="mx-auto flex w-full max-w-6xl flex-col gap-8 px-4 py-8 sm:px-6 sm:py-10 lg:px-8">
-        <CategoriesSection />
+        {/* <CategoriesSection /> */}
         {isLoading ? (
           <LoadingSpinner />
         ) : null}
